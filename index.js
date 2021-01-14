f1e89748e9440a6420c1251fc529ada2c5b0e62d
hamza <darejhamza@gmail.com>
2021-01-14 10:46:05 +0100

    passwordGen

---
+++ index.js
@@ -0,0 +1,12 @@
+// passwordGen 
+let Characters =[1,2,3,4,5,6,7,8,9,"q","w","e","r","t","z","i","o","p","a","s","d","f","g","h","j","k","l","ö","ä","y","x","c","b","n","m","!","#","§","$","%","&","/","?"];
+const passwordGen = ()=>{
+let result ="";
+let randomNum;
+for( let i=0; i <=30 ;i++){
+    randomNum = Math.floor(Math.random()* Characters.length);
+    result += Characters[randomNum]; 
+}
+return result;
+}
+console.log(passwordGen());
